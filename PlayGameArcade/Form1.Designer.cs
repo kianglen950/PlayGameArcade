f55d15fdb20be3475ef2de853ed3f9f1beb3a1ab
P4 <P4@DESKTOP-VN9LN8V>
2021-09-15 14:09:18 -0400

    Add project files.

---
+++ PlayGameArcade/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace PlayGameArcade
+{
+    partial class BackgroundForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BackgroundForm));
+            this.startButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.White;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(134, 47);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(245, 56);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start Game";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(134, 179);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(257, 52);
+            this.outputLabel.TabIndex = 1;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.outputLabel.Click += new System.EventHandler(this.outputLabel_Click);
+            // 
+            // BackgroundForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(533, 328);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BackgroundForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ PlayGameArcade/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace PlayGameArcade
+{
+    public partial class BackgroundForm : Form
+    {
+        public BackgroundForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer alertPlayer = new SoundPlayer(Properties.Resources.download);
+            SoundPlayer soundPlayer = new SoundPlayer(Properties.Resources.PowerUp);
+
+            startButton.Hide();
+
+            outputLabel.Show();
+            outputLabel.Text = "Game starting in: ";
+
+            outputLabel.Text = "Game starting in: 3";
+            Thread.Sleep(1000);
+            Refresh();
+            
+            alertPlayer.Play();
+            
+            outputLabel.Text = "Game starting in: 2";
+            Thread.Sleep(1000);
+            Refresh();
+            
+            alertPlayer.Play();
+
+            outputLabel.Text = "Game starting in: 1";
+            Thread.Sleep(1000);
+            Refresh();
+
+            alertPlayer.Play();
+           
+            outputLabel.Text = "Go!!";
+            Thread.Sleep(1000);
+            Refresh();
+
+            soundPlayer.Play();
+            outputLabel.BackColor = Color.LawnGreen;
+
+           this.BackColor = Color.LawnGreen;
+        }
+
+        private void outputLabel_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
